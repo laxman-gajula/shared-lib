f876001a0b8a8c8d0cce77953d278b73e76b0942
laxman-gajula <49583851+laxman-gajula@users.noreply.github.com>
2022-01-25 17:19:59 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,21 +1,3 @@
-@Library('jenkins-lib') _
-
-pipeline{
-    agent any
-    options {
-      timeout(30)
-    }
-    stages{
-        
-        stage('Maven'){
-             stage('Mvn Build'){
-                steps{
-                    sh 'mvn clean package'
-                }
-            }
-        
-     
-        }
-     
-    }
+def calling(name){
+    echo "hi $name}, how are you"
 }
